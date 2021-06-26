37b92ada49a8a99e3e5e3cf34c62fbb59233cbe8
Carlos Espinosa <caiespin@ucsc.edu>
2021-06-25 21:58:18 -0700

    Ya se identifican los adjetivos, agregando funcionalidad para leer los archivos de texto automaticamente

--- extraccion_adjetivos.py
+++ extraccion_adjetivos.py
@@ -1,22 +1,41 @@
 #-*- coding: utf-8 -*-
 import requests
-import io
+import json
+import glob
+from pathlib import Path
+import os
+
+#Read the text files
+currentDir = Path(os.getcwd())
+dataDir = currentDir / 'Modernismo'
+
+textFiles = glob.glob(str(dataDir)+'/*.txt',recursive=True)
+
+print(textFiles)
 #Archivo a ser enviado
 files = {'file': open("Modernismo/efren-rebolledo.txt")}
+
+
 #Parámetros
 params = {'outf': 'tagged', 'format': 'json'}
 #Enviar petición
 url = "http://www.corpus.unam.mx/servicio-freeling/analyze.php"
 r = requests.post(url, files=files, params=params)
 #Convertir de formato json
-obj = r.json()
-print(obj[0][0])
-# #Ejemplo, obtener todos los lemas
-# for sentence in obj:
-#     for word in sentence:
-#         print(word["lemma"])
-with open(r"Modernismo/efren-rebolledo.txt") as archivo:
-    texto_raw = archivo.read()
-print(texto_raw)
-adjetivos = [w for sent in obj for w in sent if w["tag"].startswith("A")]
-print(adjetivos)
+data = r.json() # lista
+
+with open('efren_rebolledo.json', 'w') as f:
+    json.dump(data, f, indent=2)
+
+# Obtener todos los adjetivos
+for entry in data:
+    for dict in entry: # cada entrada es un diccionario
+        if dict['tag'].startswith('A'): # Si el tag empiza con A es un adjetivo
+            adjective = dict['lemma']
+            print(adjective)
+
+#with open(r"Modernismo/efren-rebolledo.txt") as archivo:
+#    texto_raw = archivo.read()
+#print(texto_raw)
+#adjetivos = [w for sent in obj for w in sent if w["tag"].startswith("A")]
+#print(adjetivos)
