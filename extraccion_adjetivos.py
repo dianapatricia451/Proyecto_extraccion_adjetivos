da0660196e3fe0595482a01f56f1be7455bbada7
Pato <dianapatriciahyouka@gmail.com>
2021-07-02 03:21:56 -0500

    Extraccion de adjetivos no funciona, no se como definir data

--- extraccion_adjetivos.py
+++ extraccion_adjetivos.py
@@ -4,39 +4,18 @@ import json
 import glob
 from pathlib import Path
 import os
-
 # Get text files directories
 currentDir = Path(os.getcwd())
 dataDir = currentDir / 'Modernismo'
-textFiles = glob.glob(str(dataDir)+'/*.txt',recursive=True)
-
-# Freeling parameters
-params = {'outf': 'tagged', 'format': 'json'}
-url = "http://www.corpus.unam.mx/servicio-freeling/analyze.php"
-
+textFiles = glob.glob(str(dataDir)+'/*.json',recursive=True)
 for textfile in textFiles:
-    # Set filename for json file
-    filepath = Path(textfile)
-    author=filepath.parts[-1].split('.')[0]
-    json_filename = author+'.json'
-
     # Read text file
     f = open(textfile, "r")
     rawText = f.read()
     f.close()
-
-    # Send request to server
-    text = {'file': rawText}
-    freeling_request = requests.post(url, files=text, params=params)
-    data = freeling_request.json()
-
-    # Save server responce as json file
-    with open(json_filename, 'w') as f:
-        json.dump(data, f, indent=2)
-
-# Obtener todos los adjetivos
-# for entry in data:
-#     for dict in entry: # cada entrada es un diccionario
-#         if dict['tag'].startswith('A'): # Si el tag empiza con A es un adjetivo
-#             adjective = dict['lemma']
-#             print(adjective)
+#adjetives
+for entry in data:
+    for dict in entry: # cada entrada es un diccionario
+        if dict['tag'].startswith('A'): # Si el tag empieza con A es un adjetivo
+            adjective = dict['lemma']
+            print(adjective)    
